fcb1a6e16ce155371b497c5817724367595bc370
Cristian Yulma <cristianyulmaw@gmail.com>
2025-08-22 14:47:51 +0700

    Fixt: Fixing time stamp

--- internal/goroutine/no1.go
+++ internal/goroutine/no1.go
@@ -8,7 +8,11 @@ import (
 
 func Routine(wg *sync.WaitGroup, nama string, durasi time.Duration) {
 	defer wg.Done()
-	fmt.Printf("Mulai %s...\n", nama)
+	start := time.Now()
+	fmt.Printf("[%s] Mulai %s...\n", start.Format("15:04:05"), nama)
+
 	time.Sleep(durasi)
-	fmt.Printf("Selesai %s\n", nama)
+
+	end := time.Now()
+	fmt.Printf("[%s] Selesai %s\n", end.Format("15:04:05"), nama)
 }
